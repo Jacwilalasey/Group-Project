4867f4f49cfd15e618617d2daa1256760df327e4
James Sutcliffe <jamessutcliffe2012@hotmail.co.uk>
2023-02-10 16:51:25 +0000

    new api pulled in, search function developed - sort movie data load next

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,67 +4,81 @@ $(document).ready(function () {
 
     // Global Variables
 
-    let url = "https://movie-database-alternative.p.rapidapi.com/";
-    let apiKey = "e63e384093mshf6ec0c8a4eb5556p1c46afjsn531a89eba378";
+    let url = 'http://www.omdbapi.com/?s=';
+    let apiKey = "1fcd68b1&";
     let imageDiv = $("#genre-populate");
-    imageDiv.append('hello');
 
-    // Renders movie posters to page
+    //  Renders searched movie posters to page
 
-    function renderImages(data) {
+    function renderSearch(data) {
 
         imageDiv.empty()
         link = "www.google.com";
 
         for (let i = 0; i < data.length; i++) {
-            let posters = data[i].Poster;
-            let title = data[i].Title;
-            let description = data[i].Type;
-            var card = "<div class='col-sm-3 mb-2'>" +
-                "<div class='card'>" +
-                "<img src='" + posters + "' class='card-img-top' alt='Image'>" +
-                "<div class='card-body'>" +
-                "<h5 class='card-title'>" + title + "</h5>" +
-                "<p class='card-text'>" + description + "</p>" +
-                "<a href='" + link + "' class='btn btn-primary'>Learn More</a>" +
-                "</div>" +
-                "</div>" +
-                "</div>";
+            let poster = $('<img>').attr('src', data[i].Poster);
+            let title = $('<h1>').text(data[i].Title);
+            let type = $('<h2>').text(data[i].Type);
+            let moreInfo = $('<button>').text('More Info');
+            moreInfo.attr('data-id', data[i].imdbID);
+            moreInfo.class('')
+            let searchedMovie = $('<div>');
+            searchedMovie.append(title, poster, type, moreInfo);
 
-            imageDiv.append(card)
+            imageDiv.append(searchedMovie)
 
         }
-    }
+    };
 
+    // search function
 
-    //    genre button click funtion sets off image renders 
+    $("#search-button").click(function (event) {
+        event.preventDefault();
+        let searchInput = $('#movie-input').val().trim()
+        let queryUrl = `${url}${searchInput}&apikey=${apiKey}`;
 
-    $("button").click(function () {
-
-        var buttonId = $(this).attr("id");
-        let queryUrl = `${url}?s=${buttonId}&r=json&page=1`;
-
-        const settings = {
-            "async": true,
-            "crossDomain": true,
-            "url": queryUrl,
-            "method": "GET",
-            "headers": {
-                "X-RapidAPI-Key": apiKey,
-                "X-RapidAPI-Host": "movie-database-alternative.p.rapidapi.com"
-            }
-        };
-
-        $.ajax(settings).done(function (response) {
-            let search = response.Search
-            renderImages(search);
+        $.ajax({
+            url: queryUrl,
+            method: "GET"
         })
+            .then(function (response) {
+                console.log(response);
+
+                let search = response.Search
+                renderSearch(search);
+            });
+
+
+
+        //    genre button click funtion sets off image renders 
+
+        // $("button").click(function () {
+
+        //     var buttonId = $(this).attr("id");
+        //     let queryUrl = `${url}?s=${buttonId}&r=json&page=1`;
+
+        //     const settings = {
+        //         "async": true,
+        //         "crossDomain": true,
+        //         "url": queryUrl,
+        //         "method": "GET",
+        //         "headers": {
+        //             "X-RapidAPI-Key": apiKey,
+        //             "X-RapidAPI-Host": "movie-database-alternative.p.rapidapi.com"
+        //         }
+        //     };
+
+        //     $.ajax(settings).done(function (response) {
+        //         let search = response.Search
+        //         renderImages(search);
+        //     })
+        // });
+
+        // ty
+
     });
 
-    // ty
-
 });
-
 
 //  trying to get the randomise function working, also need the buttons populated randomly on load 
 
